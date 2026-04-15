94fd2db232eb04d988b2245cbb46ed146eb9efd4
Alan GIRALT <alangiralt@telefonica.net>
2026-04-15 21:33:06 +0200

    feat: implementation of the eratosthenesSieve() for exercise 1

--- nivell_3/exercici_1/sieve_of_eratosthenes.php
+++ nivell_3/exercici_1/sieve_of_eratosthenes.php
@@ -1,34 +1,25 @@
 <?php
 declare(strict_types=1);
-function isPrime(int $integer): bool {
-  $divisor = 2;
-  for ($divisor = 2; $divisor <= $integer; $divisor++) {
-    if ($integer % $divisor === 0) {
-      if ($integer == $divisor) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-  return false;
-}
-echo isPrime(7) . PHP_EOL;
-$numbers = [];
-for ($i = 2; $i <= 20; $i++) {
-  array_push($numbers, $i);
-}
-print_r($numbers);
-echo 'bye bye';
 function eratosthenesSieve($limit){
-  $s = ceil(sqrt($limit));
+  $s = floor(sqrt($limit));
   $integers = [];
   for ($i = 2; $i <= $limit; $i++) {
     array_push($integers, [$i, true]);
   }
-  for ($i = 0; $i <= count($integers); $i++) {
-    if ($integers[$i][0] == $s) break;
+  for ($i = 0; $i < count($integers); $i++) {
+    if ($integers[$i][0] > $s) break;
+    for ($j = $i + 1; $j < count($integers); $j++){
+      if ($integers[$j][1] && $integers[$j][0] % $integers[$i][0] == 0) {
+        $integers[$j][1] = false;
+      }
+    }
   }
-echo 'the end';
+  $primeNumbers = [];
+  for ($i = 0; $i < count($integers); $i++) {
+    if ($integers[$i][1]) {
+      array_push($primeNumbers, $integers[$i][0]);
+    }
+  }
+  return $primeNumbers;
 }
-eratosthenesSieve(16);
+print_r(eratosthenesSieve(100));
