257681e99496ea4fd47672697a326241a0c40aa3
Alan GIRALT <alangiralt@telefonica.net>
2026-04-15 13:51:22 +0200

    feat: unfinished implementation of the eratosthenes Sieve() function flowchart

--- nivell_3/exercici_1/sieve_of_eratosthenes.php
+++ nivell_3/exercici_1/sieve_of_eratosthenes.php
@@ -13,8 +13,22 @@ function isPrime(int $integer): bool {
   }
   return false;
 }
+echo isPrime(7) . PHP_EOL;
 $numbers = [];
 for ($i = 2; $i <= 20; $i++) {
   array_push($numbers, $i);
 }
 print_r($numbers);
+echo 'bye bye';
+function eratosthenesSieve($limit){
+  $s = ceil(sqrt($limit));
+  $integers = [];
+  for ($i = 2; $i <= $limit; $i++) {
+    array_push($integers, [$i, true]);
+  }
+  for ($i = 0; $i <= count($integers); $i++) {
+    if ($integers[$i][0] == $s) break;
+  }
+echo 'the end';
+}
+eratosthenesSieve(16);
